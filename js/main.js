7771bbcd1079dee3e39670b78f39a69b3f6b3a39
belenespilman <belen.espilman@gmail.com>
2023-10-15 22:19:10 -0300

    Project setup

---
+++ js/main.js
@@ -0,0 +1,186 @@
+// PRODUCTS
+const products = [
+    // Rings
+    {
+        id: "ring-01",
+        title: "Ring 01",
+        image: "./img/rings/01.jpg",
+        category: {
+            name: "Rings",
+            id: "rings"
+        },
+        price: 1000
+    },
+    {
+        id: "ring-02",
+        title: "Ring 02",
+        image: "./img/rings/02.jpg",
+        category: {
+            name: "Rings",
+            id: "rings"
+        },
+        price: 1000
+    },
+    {
+        id: "ring-03",
+        title: "Ring 03",
+        image: "./img/rings/03.jpg",
+        category: {
+            name: "Rings",
+            id: "rings"
+        },
+        price: 1000
+    },
+    {
+        id: "ring-04",
+        title: "Ring 04",
+        image: "./img/rings/04.jpg",
+        category: {
+            name: "Rings",
+            id: "rings"
+        },
+        price: 1000
+    },
+    {
+        id: "ring-05",
+        title: "Ring 05",
+        image: "./img/rings/05.jpg",
+        category: {
+            name: "Rings",
+            id: "rings"
+        },
+        price: 1000
+    },
+    // Necklaces 
+    {
+        id: "necklace-01",
+        title: "Necklace 01",
+        image: "./img/necklaces/01.jpg",
+        category: {
+            name: "Necklaces",
+            id: "necklaces"
+        },
+        price: 1000
+    },
+    {
+        id: "necklace-02",
+        title: "Necklace 02",
+        image: "./img/necklaces/02.jpg",
+        category: {
+            name: "Necklaces",
+            id: "necklaces"
+        },
+        price: 1000
+    },
+    {
+        id: "necklace-03",
+        title: "Necklace 03",
+        image: "./img/necklaces/03.jpg",
+        category: {
+            name: "Necklaces",
+            id: "necklaces"
+        },
+        price: 1000
+    },
+    {
+        id: "necklace-04",
+        title: "Necklace 04",
+        image: "./img/necklaces/04.jpg",
+        category: {
+            name: "Necklaces",
+            id: "necklaces"
+        },
+        price: 1000
+    },
+    {
+        id: "necklace-05",
+        title: "Necklace 05",
+        image: "./img/necklaces/05.jpg",
+        category: {
+            name: "Necklaces",
+            id: "necklaces"
+        },
+        price: 1000
+    },
+    {
+        id: "necklace-06",
+        title: "Necklace 06",
+        image: "./img/necklaces/06.jpg",
+        category: {
+            name: "Necklaces",
+            id: "necklaces"
+        },
+        price: 1000
+    },
+    {
+        id: "necklace-07",
+        title: "Necklace 07",
+        image: "./img/necklaces/07.jpg",
+        category: {
+            name: "Necklaces",
+            id: "necklaces"
+        },
+        price: 1000
+    },
+    {
+        id: "necklace-08",
+        title: "Necklace 08",
+        image: "./img/necklaces/08.jpg",
+        category: {
+            name: "Necklaces",
+            id: "necklaces"
+        },
+        price: 1000
+    },
+    // Earings
+    {
+        id: "earing-01",
+        title: "Earing 01",
+        image: "./img/earings/01.jpg",
+        category: {
+            name: "Earings",
+            id: "earings"
+        },
+        price: 1000
+    },
+    {
+        id: "earing-02",
+        title: "Earing 02",
+        image: "./img/earings/02.jpg",
+        category: {
+            name: "Earings",
+            id: "earings"
+        },
+        price: 1000
+    },
+    {
+        id: "earing-03",
+        title: "Earing 03",
+        image: "./img/earings/03.jpg",
+        category: {
+            name: "Earings",
+            id: "earings"
+        },
+        price: 1000
+    },
+    {
+        id: "earing-04",
+        title: "Earing 04",
+        image: "./img/earings/04.jpg",
+        category: {
+            name: "Earings",
+            id: "earings"
+        },
+        price: 1000
+    },
+    {
+        id: "earing-05",
+        title: "Earing 05",
+        image: "./img/earings/05.jpg",
+        category: {
+            name: "Earings",
+            id: "earings"
+        },
+        price: 1000
+    }
+];
